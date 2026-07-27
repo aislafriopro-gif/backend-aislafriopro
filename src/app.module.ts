2a5df4fd564241cfd685079376204dc5c78f38a0
GFGaldeano <gustavo_galdeano@yahoo.com.ar>
2026-07-27 15:49:28 -0300

    feat(auth): integrate AuthModule into application (SCRUM-78)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import configuration, {
   ApplicationConfiguration,
   validateEnvironment,
@@ -49,6 +50,7 @@ import { HealthModule } from './health/health.module';
     RolesModule,
     ServicesModule,
     HealthModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
